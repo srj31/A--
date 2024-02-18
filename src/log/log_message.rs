85ee9370c9c4f12147c5391359d67f28a8d53ec5
Sourabh Jaiswal <sourabhrj31@gmail.com>
2024-02-18 15:40:44 +0800

    :add: log message with color

--- src/log/log_message.rs
+++ src/log/log_message.rs
@@ -1,9 +1,10 @@
 use crate::log::color_helper::{text_message_with_color, BasicColor, Color};
 
 pub fn print_error_msg(msg: &str) {
-    let error_msg = format!("Error: {}", msg);
-    println!(
-        "{}",
-        text_message_with_color(Color::Basic(BasicColor::Red), &error_msg)
+    let error_msg = format!(
+        "{}: {}",
+        text_message_with_color(Color::Bold(BasicColor::Red), "Error"),
+        text_message_with_color(Color::Basic(BasicColor::Red), msg)
     );
+    println!("{}", error_msg);
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,11 @@ use log::log_message::print_error_msg;
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
-        2 => println!("{}", args[1]),
+        2 => execute_file(&args[1]),
         _ => print_error_msg("Usage: amm [File]"),
     }
+}
+
+fn execute_file(file: &str) {
+    todo!()
 }
